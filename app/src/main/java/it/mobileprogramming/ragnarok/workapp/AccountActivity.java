d9fb499be16dcfab4cd81a0f6435febc1ab0724a
Paride Casulli <paride.casulli@gmail.com>
2015-08-03 21:02:52 +0200

    added a first draft of account activity, added user field in app.java singletons class

--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/AccountActivity.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/AccountActivity.java
@@ -1,12 +1,30 @@
 package it.mobileprogramming.ragnarok.workapp;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+import it.mobileprogramming.ragnarok.workapp.GymModel.SQLiteSerializer;
+import it.mobileprogramming.ragnarok.workapp.GymModel.User;
+import it.mobileprogramming.ragnarok.workapp.GymModel.WeightItem;
+import it.mobileprogramming.ragnarok.workapp.util.App;
 import it.mobileprogramming.ragnarok.workapp.util.BaseActivityWithToolbar;
 
 public class AccountActivity extends BaseActivityWithToolbar {
+    TextView tvTop;
+    User currentUser;
+    LineChart chart;
 
     @Override
     protected int getLayoutResourceId() {
@@ -33,5 +51,54 @@ public class AccountActivity extends BaseActivityWithToolbar {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SQLiteSerializer dbSerializer = ((App) this.getApplication()).getDBSerializer();
+        dbSerializer.open();
+
+        //TODO TO BE REMOVED
+        User anUser = dbSerializer.loadUser(1);
+        ((App) this.getApplication()).setCurrentUser(anUser);
+        //END TO BE REMOVED
+
+        currentUser = ((App) this.getApplication()).getCurrentUser();
+        tvTop   =  (TextView)findViewById(R.id.tv_top);
+        tvTop.setText(currentUser.getStrName()+" "+currentUser.getStrSurname());
+        chart = (LineChart) findViewById(R.id.lc_chart);
+        chart.setDescription(getString(R.string.account_trend));
+        ArrayList<WeightItem> history = currentUser.getWeightHistory();
+        LineData line = new LineData();
+        ArrayList<Entry> dataset = new ArrayList<Entry>();
+        ArrayList<String>labels =   new ArrayList<String>();    //entry labels
+        for (int i = 0; i < history.size(); i++) {
+            WeightItem anItem   =   history.get(i);
+            dataset.add(new Entry(anItem.value, i));
+            labels.add(anItem.date.getDay()+"/"+anItem.date.getMonth()+"/"+anItem.date.getYear());
+        }
+        LineDataSet aDataset = new LineDataSet(dataset, currentUser.getStrName());
+
+        TypedValue a = new TypedValue();
+        int color;
+        getTheme().resolveAttribute(android.R.attr.colorPrimary, a, true);//TODO works only from API 21!!!
+        if (a.type >= TypedValue.TYPE_FIRST_COLOR_INT && a.type <= TypedValue.TYPE_LAST_COLOR_INT) {
+            // windowBackground is a color
+            color = a.data;
+        } else {
+            // windowBackground is not a color, probably a drawable
+            color   = Color.BLACK;
+        }
+
+
+
+        aDataset.setColor(color);
+        aDataset.setCircleColor(color);
+        line.addDataSet(aDataset);
+        ArrayList<LineDataSet> datasets =   new ArrayList<LineDataSet>();
+        datasets.add(aDataset);
+        LineData data=  new LineData(labels,datasets);
+        chart.setData(data);
     }
 }
--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/util/App.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/util/App.java
@@ -3,6 +3,7 @@ package it.mobileprogramming.ragnarok.workapp.util;
 import android.app.Application;
 
 import it.mobileprogramming.ragnarok.workapp.GymModel.SQLiteSerializer;
+import it.mobileprogramming.ragnarok.workapp.GymModel.User;
 
 /**
  * Own implementation of Application with collects singletons.
@@ -12,6 +13,7 @@ public class App extends Application {
      * SQLiteSerializer instance.
      */
     private SQLiteSerializer sqLiteSerializer;
+    private User currentUser;                   //current user logged in the application
 
     /**
      * Singleton for SQLiteSerializer.
@@ -23,5 +25,22 @@ public class App extends Application {
             sqLiteSerializer = new SQLiteSerializer(this, "workapp.db");
         }
         return sqLiteSerializer;
+    }
+
+    /**
+     * gets the current user of the application
+     * @return
+     */
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    /**#
+     * sets the current user of this application
+     * @param currentUser current user of the app
+     */
+    public void setCurrentUser(User currentUser) {
+        //TODO check with authentication!!! If leaved in this way is a security issue!
+        this.currentUser = currentUser;
     }
 }
