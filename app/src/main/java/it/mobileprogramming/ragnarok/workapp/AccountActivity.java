7859aa6e7e43f5cbab88601ec885506e1d0e7346
pincopallino93 <pastorini.claudio@gmail.com>
2015-08-18 17:50:47 +0200

    Fix bug no menu resource

--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/AccountActivity.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/AccountActivity.java
@@ -43,29 +43,6 @@ public class AccountActivity extends BaseActivityWithToolbar {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_account_activty, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
