b8d03bb26c7e6ad10f32b5fc492b9e4e20dceddd
Paride Casulli <paride.casulli@gmail.com>
2015-08-28 12:01:47 +0200

    fixed icon issue, added stars icons to GIT

--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/FeedbackActivity.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/FeedbackActivity.java
@@ -47,11 +47,11 @@ public class FeedbackActivity extends ActionBarActivity {
         if(value>-1){
             for(int i=0;i<value;i++){
                 if(i<5) {
-                    stars.get(i).setImageResource(R.drawable.ic_launcher);
+                    stars.get(i).setImageResource(R.drawable.goldstar);
                 }
             }
             for(int i=value;i<stars.size();i++){
-                stars.get(i).setImageResource(R.drawable.ic_launcher);
+                stars.get(i).setImageResource(R.drawable.greystar);
             }
         }
         this.commentableItem.setRating(value);
