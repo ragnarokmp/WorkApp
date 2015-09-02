a99cf383047ccb72e095ea636e0048ba15b8220f
Paride Casulli <paride.casulli@gmail.com>
2015-09-02 20:00:23 +0200

    improved startexerciseactivity

--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/StartExerciseActivity.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/StartExerciseActivity.java
@@ -42,6 +42,7 @@ public class StartExerciseActivity extends BaseActivity {
 
     boolean paused  =   false;
     boolean stopped =   false;
+    GraphManager aManager;
 
     private class GraphManager extends AsyncTask{
         long previous;
@@ -53,7 +54,7 @@ public class StartExerciseActivity extends BaseActivity {
                 SystemClock.sleep(100);
                 long now = System.currentTimeMillis();
                 now =   now-previous;
-                System.out.println("MILLISECONDS DELTA"+now);
+                System.out.println("MILLISECONDS DELTA"+now+" PROGRESS "+progress);
                 //System.out.println("paused status " + paused);
                 if(paused==false&&stopped==false){
                     Object[] payload    =   new Object[1];
@@ -111,16 +112,25 @@ public class StartExerciseActivity extends BaseActivity {
         milliseconds = 1000;
         totalMillis = 15 * milliseconds;
 
+        if(savedInstanceState!=null){
+            progress            =   savedInstanceState.getInt("progress");
+            this.milliseconds   =   savedInstanceState.getInt("milliseconds");
+            this.totalMillis    =   savedInstanceState.getInt("totalMillis");
+            this.paused         =   savedInstanceState.getBoolean("paused");
+            this.stopped         =   savedInstanceState.getBoolean("stopped");
+        }
+
         // Get DecoView
         decoView = (DecoView) findViewById(R.id.deco_view);
         seriesItem = new SeriesItem.Builder(getResources().getColor(R.color.accent))
                 .setRange(0, totalMillis, 0)
                 .setLineWidth(32f)
+                .setSpinDuration(101)
                 .build();
 
         decoView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
-                .setDelay(1000)
-                .setDuration(2000)
+                .setDelay(0)
+                .setDuration(0)
                 .build());
 
         // Get Views
@@ -129,6 +139,22 @@ public class StartExerciseActivity extends BaseActivity {
         final View layout = findViewById(R.id.frame_layout);
         final ImageView pauseImageView = (ImageView) findViewById(R.id.pause_image_view);
         final ImageView stopImageView = (ImageView) findViewById(R.id.stop_image_view);
+
+        //restore pause button state
+        if(paused==true){
+            // Get drawable
+            Drawable drawable;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                drawable = getDrawable(R.drawable.ic_action_play);
+            } else {
+                drawable = getResources().getDrawable(R.drawable.ic_action_play);
+            }
+
+            // Set the new drawable and other stuffs..
+            pauseImageView.setImageDrawable(drawable);
+            pauseImageView.setContentDescription(getString(R.string.play));
+            pauseImageView.setTag(getString(R.string.play));
+        }
 
         // Group some of them for final animation
         linkedViews = new View[]{textViewPercentage, textViewRemaining, layout};
@@ -151,7 +177,10 @@ public class StartExerciseActivity extends BaseActivity {
                     } else {
                         drawable = getResources().getDrawable(R.drawable.ic_action_pause);
                     }
-
+                    if(progress==0){
+                        aManager = new GraphManager();
+                        aManager.execute();
+                    }
                     // Set the new drawable and other stuffs..
                     pauseImageView.setImageDrawable(drawable);
                     pauseImageView.setContentDescription(getString(R.string.pause));
@@ -180,49 +209,50 @@ public class StartExerciseActivity extends BaseActivity {
             public void onClick(View v) {
                 Log.i(TAG, "onClick!");
                 Log.i(TAG, "Con tag= " + String.valueOf(stopImageView.getTag()));
-
-                if (stopImageView.getTag().equals(getString(R.string.play)) && pauseImageView.getTag().equals(getString(R.string.pause))) {
-                    Log.i(TAG, "PLAY");
-                    //TODO implement actions
-                    // Get drawable
-                    Drawable drawable;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        drawable = getDrawable(R.drawable.ic_action_stop);
-                    } else {
-                        drawable = getResources().getDrawable(R.drawable.ic_action_stop);
-                    }
-
-                    // Set the new drawable and other stuffs..
-                    stopImageView.setImageDrawable(drawable);
-                    stopImageView.setContentDescription(getString(R.string.stop));
-                    stopImageView.setTag(getString(R.string.stop));
-                } else if (stopImageView.getTag().equals(getString(R.string.stop)) && pauseImageView.getTag().equals(getString(R.string.pause))) {
-                    Log.i(TAG, "STOP");
-                    //TODO implement actions
-                    stopped =   true;
-                    progress    =   0;
-                    decoView.executeReset();
-                    int series1Index = decoView.addSeries(seriesItem);
-                    decoView.addEvent(new DecoEvent.Builder(progress).setIndex(series1Index).setDelay(0).build());
-                    textViewPercentage.setText(String.valueOf((int) progress / milliseconds));
-                    textViewRemaining.setText(new SimpleDateFormat("mm:ss:SSS", Locale.ITALY).format(new Date((long) progress)));
-                    // Get drawable
-                    Drawable drawable;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        drawable = getDrawable(R.drawable.ic_action_play);
-                    } else {
-                        drawable = getResources().getDrawable(R.drawable.ic_action_play);
-                    }
-
-                    // Set the new drawable and other stuffs..
-                    stopImageView.setImageDrawable(drawable);
-                    stopImageView.setContentDescription(getString(R.string.play));
-                    stopImageView.setTag(getString(R.string.play));
+                Log.i(TAG, "STOP");
+                //TODO implement actions
+                stopped =   true;
+                progress    =   0;
+                decoView.executeReset();
+                int series1Index = decoView.addSeries(seriesItem);
+                decoView.addEvent(new DecoEvent.Builder(progress).setIndex(series1Index).setDelay(0).build());
+                textViewPercentage.setText(String.valueOf((int) progress / milliseconds));
+                textViewRemaining.setText(new SimpleDateFormat("mm:ss:SSS", Locale.ITALY).format(new Date((long) progress)));
+                // Get drawable
+                Drawable drawable;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    drawable = getDrawable(R.drawable.ic_action_play);
+                } else {
+                    drawable = getResources().getDrawable(R.drawable.ic_action_play);
                 }
+                // Set the new drawable and other stuffs..
+                pauseImageView.setImageDrawable(drawable);
+                pauseImageView.setContentDescription(getString(R.string.play));
+                pauseImageView.setTag(getString(R.string.play));
             }
         });
-        GraphManager aManager   =   new GraphManager();
-        aManager.execute();
 
+        if(savedInstanceState!=null){
+            int series1Index = decoView.addSeries(seriesItem);
+            if(progress>totalMillis){
+                progress    =   totalMillis;
+             }
+            decoView.addEvent(new DecoEvent.Builder(progress).setIndex(series1Index).setDelay(0).build());
+            textViewPercentage.setText(String.valueOf((int) progress / milliseconds));
+            textViewRemaining.setText(new SimpleDateFormat("mm:ss:SSS", Locale.ITALY).format(new Date((long) progress)));
+        }
+        aManager = new GraphManager();
+        aManager.execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("progress", progress);
+        outState.putInt("milliseconds", milliseconds);
+        outState.putInt("totalMillis", totalMillis);
+        outState.putBoolean("paused",paused);
+        outState.putBoolean("stopped",stopped);
+        stopped =   true;
     }
 }
