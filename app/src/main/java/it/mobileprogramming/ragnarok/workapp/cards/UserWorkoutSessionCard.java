a46d204b34076bb2b723816bfa4167ab53f320d7
Andrea Cerra <andrea.cerra@me.com>
2015-09-08 13:04:55 +0200

    Refactor,  UserWorkoutSessionCard is used for object UserWorkoutSession

---
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/cards/UserWorkoutSessionCard.java
@@ -0,0 +1,47 @@
+package it.mobileprogramming.ragnarok.workapp.cards;
+
+import android.content.Context;
+
+import com.dexafree.materialList.cards.SimpleCard;
+
+import it.mobileprogramming.ragnarok.workapp.GymModel.UserWorkoutSession;
+import it.mobileprogramming.ragnarok.workapp.R;
+
+/**
+ * Card for WorkoutSession.
+ */
+public class UserWorkoutSessionCard extends SimpleCard {
+
+    /**
+     * Indicates if the Session is complete or not.
+     */
+    private boolean complete;
+
+    public UserWorkoutSessionCard(Context context, UserWorkoutSession workoutSession) {
+        super(context);
+        // Set the tag of the card as explained here https://github.com/dexafree/MaterialList/wiki/Recovering-data-from-the-Cards
+        this.setTag(workoutSession);
+    }
+
+    @Override
+    public int getLayout() {
+        // Set the right layout of the card
+        return R.layout.user_workout_session_card_layout;
+    }
+
+    /**
+     * Getter for complete attribute
+     * @return true if the session is complete, false otherwise.
+     */
+    public boolean isComplete() {
+        return complete;
+    }
+
+    /**
+     * Setter for complete attribute.
+     * @param complete true if the session is complete, false otherwise.
+     */
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
