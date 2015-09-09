a08775e9af58df045d76b55e21e5e73e57cf371f
Andrea Cerra <andrea.cerra@me.com>
2015-09-09 18:01:31 +0200

    Added activity for Workout creation

--- app/src/main/java/it/mobileprogramming/ragnarok/workapp/WorkoutCreateActivity.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/WorkoutCreateActivity.java
@@ -1,7 +1,31 @@
 package it.mobileprogramming.ragnarok.workapp;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.dexafree.materialList.controller.RecyclerItemClickListener;
+import com.dexafree.materialList.model.CardItemView;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+import java.util.Date;
+
+import it.mobileprogramming.ragnarok.workapp.GymModel.SQLiteSerializer;
+import it.mobileprogramming.ragnarok.workapp.GymModel.UserWorkoutSession;
+import it.mobileprogramming.ragnarok.workapp.GymModel.WorkoutSession;
+import it.mobileprogramming.ragnarok.workapp.cards.UserWorkoutSessionCreateCard;
+import it.mobileprogramming.ragnarok.workapp.util.App;
 import it.mobileprogramming.ragnarok.workapp.util.BaseActivityWithToolbar;
+import it.mobileprogramming.ragnarok.workapp.util.MyMaterialListView;
 
 public class WorkoutCreateActivity extends BaseActivityWithToolbar {
 
@@ -12,7 +36,74 @@ public class WorkoutCreateActivity extends BaseActivityWithToolbar {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        // Get MaterialListView
+        final MyMaterialListView workoutListView = (MyMaterialListView) findViewById(R.id.session_list_view);
+
+        // Get divider for MaterialListView
+        Drawable drawable;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            drawable = getDrawable(R.drawable.divider);
+        } else {
+            //noinspection deprecation
+            drawable = getResources().getDrawable(R.drawable.divider);
+        }
+
+        // Set divider to MaterialListView
+        workoutListView.setDivider(drawable); //TODO doesn't work well in landscape mode..
+
+        // Set onItemTouchListener
+        workoutListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(CardItemView cardItemView, int i) {
+
+            }
+
+            @Override
+            public void onItemLongClick(CardItemView cardItemView, int i) {
+
+            }
+        });
+
+        //UserWorkout newWorkout = new UserWorkout("Workout a runtime",1,"test","facile",dbSerializer,dbSerializer);
+        //newWorkout.addWorkoutSession(newSession,true,0);
+        //Exercise ex1  =   new Exercise(dbSerializer,2,"esercizio1",1,1,1,"nulla","bicipite");
+        //UserExercise anExercise =   new UserExercise(1,new Date(),ex1,false,"so sempre bone",dbSerializer,dbSerializer,2);
+        //newSession.addExerciseToWorkoutSession(anExercise, 0, true);
+        //Exercise ex2  =   new Exercise(dbSerializer,2,"esercizio2",1,1,1,"nulla","gambe");
+        //UserExercise anExercise2 =   new UserExercise(1,new Date(),ex2,false,"ce piace",dbSerializer,dbSerializer,3);
+        //newSession.addExerciseToWorkoutSession(anExercise2, 0, true);
+
+        /*FloatingActionButton addWorkout = (FloatingActionButton) findViewById(R.id.action_add);
+        addWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(this, WorkoutCreateActivity.class);
+                //startActivity(intent);
+            }
+        });*/
+
+        final SQLiteSerializer dbSerializer = ((App) getApplication()).getDBSerializer();
+        dbSerializer.open();
+
+        Button addWorkout = (Button) findViewById(R.id.button);
+        addWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View move = findViewById(R.id.moveLayout);
+                if (move.getVisibility() == View.VISIBLE)
+                    move.setVisibility(View.GONE);
+
+                WorkoutSession testSession = new WorkoutSession("",dbSerializer);
+                UserWorkoutSession newSession = new UserWorkoutSession("",1,dbSerializer,dbSerializer,new Date(),testSession.getId(),"",true,0);
+
+                UserWorkoutSessionCreateCard card = new UserWorkoutSessionCreateCard(getApplicationContext(), newSession);
+                workoutListView.add(card);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/it/mobileprogramming/ragnarok/workapp/cards/UserWorkoutSessionCreateCardItemView.java
@@ -0,0 +1,295 @@
+package it.mobileprogramming.ragnarok.workapp.cards;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dexafree.materialList.cards.OnButtonPressListener;
+import com.dexafree.materialList.model.Card;
+import com.dexafree.materialList.model.CardItemView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+import it.mobileprogramming.ragnarok.workapp.ExeciseListActivityCheckbox;
+import it.mobileprogramming.ragnarok.workapp.ExerciseListActivity;
+import it.mobileprogramming.ragnarok.workapp.GymModel.Exercise;
+import it.mobileprogramming.ragnarok.workapp.GymModel.UserWorkoutSession;
+import it.mobileprogramming.ragnarok.workapp.R;
+
+/**
+ * View for WorkoutSessionCard.
+ */
+public class UserWorkoutSessionCreateCardItemView extends CardItemView<UserWorkoutSessionCreateCard> {
+    /**
+     * Divider margin for Android guidelines.
+     */
+    private final static int DIVIDER_MARGIN_DP = 16;
+
+    /**
+     * The session bound with the card.
+     */
+    private UserWorkoutSession workoutSession;
+
+    // Default constructors
+    public UserWorkoutSessionCreateCardItemView(Context context) {
+        super(context);
+    }
+
+    public UserWorkoutSessionCreateCardItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UserWorkoutSessionCreateCardItemView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void build(UserWorkoutSessionCreateCard card) {
+
+        super.build(card);
+
+        // Get workout session from the card
+        workoutSession = (UserWorkoutSession) card.getTag();
+        // Choose session image from assets evaluating the session
+        Drawable sessionDrawable = chooseSessionDrawable();
+        // Generate session description with exercises and muscle used
+        String sessionDescription = generateDescription();
+
+        // Set all stuffs
+        setImage(sessionDrawable);
+        setDescription(sessionDescription);
+        setTitles();
+        //setDuration();
+        setExercises();
+        //setCompletion();
+        setDivider(true, true);
+        setButtons(true, card);
+    }
+
+    /**
+     * This method choose an image to show of the session according to exercises.
+     * @return the chosen drawable image.
+     */
+    private Drawable chooseSessionDrawable() {
+
+        int resourceId = getResources().getIdentifier("exercise_" + String.valueOf(new Random().nextInt(7) + 1), "raw", getContext().getPackageName());
+
+        Drawable drawable;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            drawable = getContext().getDrawable(resourceId);
+        } else {
+            drawable = getContext().getResources().getDrawable(resourceId);
+        }
+
+        return drawable;
+    }
+
+    /**
+     * This method allows to set the completion of the session.
+     */
+    private void setCompletion() {
+        TextView completionTextView = (TextView) findViewById(R.id.completion_text_view);
+        try {
+            completionTextView.setText(String.valueOf(workoutSession.allExerciseDone()) + "%");  //TODO is a try/catch correct?
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method allows to set the total number of exercise of the session.
+     */
+    private void setExercises() {
+        ArrayList<Exercise> exercises = workoutSession.getExercisesOfSession();
+        TextView exercisesTextView = (TextView) findViewById(R.id.exercises_text_view);
+        exercisesTextView.setText(String.valueOf(exercises.size()));
+    }
+
+    /**
+     * This method allows to set session total duration (duration of exercises + recovery time).
+     */
+    private void setDuration() {
+        int totalTime = 0;
+        ArrayList<Exercise> exercises = workoutSession.getExercisesOfSession();
+        for(int i = 0; i < exercises.size(); i++) {
+            Exercise exercise = exercises.get(i);
+
+            int timeForSeries = 0;
+            timeForSeries += exercise.getFrequency() * exercise.getRepetition();
+            int recoveryTime = 0;
+            recoveryTime += exercise.getRecovery();
+            totalTime += (timeForSeries + recoveryTime) * exercise.getSeries();
+        }
+        TextView durationTextView = (TextView) findViewById(R.id.duration_text_view);
+        durationTextView.setText("~" + totalTime / 60 + " min");
+    }
+
+    /**
+     * This method allows to set the overlays titles.
+     */
+    private void setTitles() {
+
+        //TextView durationTitleTextView = (TextView) findViewById(R.id.duration_title_text_view);
+        //durationTitleTextView.setText(getResources().getString(R.string.duration_title).toUpperCase());
+
+        TextView exercisesTitleTextView = (TextView) findViewById(R.id.exercises_title_text_view);
+        exercisesTitleTextView.setText(getResources().getString(R.string.exercises_title).toUpperCase());
+
+        //TextView completionTitleTextView = (TextView) findViewById(R.id.completion_title_text_view);
+        //completionTitleTextView.setText(getResources().getString(R.string.completion_title).toUpperCase());
+    }
+
+    /**
+     * This method allows to set session image.
+     * @param image the image to set.
+     */
+    public void setImage(Drawable image) {
+        ImageView sessionImageView = (ImageView) findViewById(R.id.session_image_view);
+        sessionImageView.setImageDrawable(image);
+    }
+
+    /**
+     * This method generates description of the session according to exercises.
+     * @return the description in a human readable way.
+     */
+    private String generateDescription() {
+
+        ArrayList<Exercise> exercises = workoutSession.getExercisesOfSession();
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        String description;
+
+        //TODO: @federico improve with the recognition of the actual day...
+        if ((workoutSession.getDateSessionDate().getTime() - date.getTime()) == 1) {
+            description = "$Tomorrow$ we will work on $";
+        } else if ((workoutSession.getDateSessionDate().getTime() - date.getTime()) == 2) {
+            description = "$The day after tomorrow we will work on $";
+        } else {
+            description = "In the session of $" + workoutSession.getDateSessionDate() + "$ we will work on $";
+        }
+        int size = exercises.size();
+        for (int i = 0; i < size; i++) {
+            String muscle = exercises.get(i).getMuscles();
+            muscle = muscle.substring(0,muscle.length()-2); //There is \n escape character at the end
+            description += muscle;
+            if (i < size - 2) {
+                description += ", ";
+            }
+            if (i == size - 2) {
+                description += " and ";
+            }
+        }
+        description += "$.\n\nIn details:\n";
+        for (int i = 0; i < size; i++) {
+            Exercise currentEx = exercises.get(i);
+            description += " $• " + currentEx.getSeries() + "$ series of $" + currentEx.getName() + "$";
+            if (size > 1 && i != size - 1) {
+                description += "\n";
+            }
+        }
+        return description;
+    }
+
+    /**
+     * This method allows to set the buttons.
+     * @param ButtonsVisible true if the buttons have to be visible, false otherwise.
+     * @param card the card.
+     */
+    private void setButtons(Boolean ButtonsVisible, final Card card) {
+
+        final TextView startNowTextButton = (TextView) findViewById(R.id.start_now_text_button);
+        startNowTextButton.setVisibility(ButtonsVisible ? VISIBLE : GONE);
+        final TextView detailsTextButton = (TextView) findViewById(R.id.details_text_button);
+        detailsTextButton.setVisibility(ButtonsVisible ? VISIBLE : GONE);
+
+        startNowTextButton.setText(getResources().getString(R.string.start_now_button).toUpperCase());
+        startNowTextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new OnButtonPressListener() {
+                    @Override
+                    public void onButtonPressedListener(View view, Card card) {
+                        Intent intent = new Intent(getContext(), ExeciseListActivityCheckbox.class);
+                        getContext().startActivity(intent);
+                    }
+                }.onButtonPressedListener(startNowTextButton, card);
+            }
+        });
+
+        detailsTextButton.setText(getResources().getString(R.string.details_button).toUpperCase());
+        detailsTextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new OnButtonPressListener() {
+                    @Override
+                    public void onButtonPressedListener(View view, Card card) {
+                        Intent intent = new Intent(getContext(), ExerciseListActivity.class);
+                        getContext().startActivity(intent);
+                    }
+                }.onButtonPressedListener(detailsTextButton, card);
+            }
+        });
+    }
+
+    /**
+     * This method allows to set the divider.
+     * @param dividerVisible true if the divider has to be visible, false otherwise.
+     * @param fullWidthDivider true if the divider must occupy all width's card, false otherwise.
+     */
+    private void setDivider(Boolean dividerVisible, Boolean fullWidthDivider) {
+        View divider = findViewById(R.id.card_divider);
+        divider.setVisibility(dividerVisible ? VISIBLE : GONE);
+
+        // After setting the visibility, we prepare the divider params according to the preferences
+        if (dividerVisible) {
+            // If the divider has to be from side to side, the margin will be 0
+            if (fullWidthDivider) {
+                ((ViewGroup.MarginLayoutParams) divider.getLayoutParams()).setMargins(0, 0, 0, 0);
+            } else {
+                int dividerMarginPx = (int) dpToPx(DIVIDER_MARGIN_DP);
+                // Set the margin
+                ((ViewGroup.MarginLayoutParams) divider.getLayoutParams()).setMargins(dividerMarginPx, 0, dividerMarginPx, 0);
+            }
+        }
+    }
+
+    /**
+     * This method allow to set the description text.
+     * @param description the description text to set.
+     */
+    private void setDescription(String description) {
+        TextView descriptionTextView = (TextView) findViewById(R.id.description_text_view);
+        descriptionTextView.setVisibility(GONE);
+        //descriptionTextView.setText(boldTextBetweenTokens(description, "$"));
+    }
+
+    /**
+     * Utility method that converts dp to px.
+     * @param dp the dp to convert in px.
+     * @return the conversion in px of the dp.
+     */
+    private float dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    /**
+     * Utility method that converts sp to px.
+     * @param sp the sp to convert in px.
+     * @return the conversion in px of the sp.
+     */
+    private float spToPx(int sp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(sp * (displayMetrics.scaledDensity / DisplayMetrics.DENSITY_DEFAULT));
+    }
+}
